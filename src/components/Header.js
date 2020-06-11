1abf8955e208f741f71bec57f3a0d5ba0bbce942
Tsuckan <tsuckanovdmitriy@gmail.com>
2020-06-11 15:43:02 +0300

    Start

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () =>
+{
+    return(
+        <header>
+            <img src="/sample_logo.png" alt=""/>
+        </header>
+    )
+}
+
+export default Header;
